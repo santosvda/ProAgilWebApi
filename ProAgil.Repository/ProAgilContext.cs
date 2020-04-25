42092b476b5eec32a8990ae7585bb070de24218e
santosvda <felipesantosacct@gmail.com>
2020-04-25 15:39:52 -0300

    gdf

--- ProAgil.Repository/ProAgilContext.cs
+++ ProAgil.Repository/ProAgilContext.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 
 namespace ProAgil.Repository
 {
-    public class ProAgilContext : DbContext
+    public class ProAgilContext : IdentityDbContext<User, Role, int,
+                                                    IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+                                                    IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public ProAgilContext(DbContextOptions<ProAgilContext> options) : base (options){ }
 
@@ -12,9 +17,37 @@ namespace ProAgil.Repository
         public DbSet<PalestranteEvento> PalestrantesEventos { get; set; }
         public DbSet<Lote> Lotes { get; set; }
         public DbSet<RedeSocial> RedesSociais { get; set; }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole =>
+                {
+                    userRole.HasKey(ur => new{ur.UserId, ur.RoleId});
+
+
+                    /*
+                    Custei a enteder esta merda
+                    o relacionamento de n-n é dado por mei odas chaves estrangeiras dentro de UseRole
+                    que vem de Role e User e são obrigatorias
+                    */
+                    userRole.HasOne(ur => ur.Role)
+                    .WithMany(r =>UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                    userRole.HasOne(ur => ur.User)
+                    .WithMany(r =>UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+
+                    /*
+                    Custei a enteder esta merda
+
+                    */
+                }
+            );
             /*
                 sobrescrevendo o metodo
                 especificando a relação (n,n) Evento - Palestrante 
